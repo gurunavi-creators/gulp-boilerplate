485eece75577a042df091e9b4f5aaec1758d697d
Ryo Akiyama <akiyama-r@gnavi.co.jp>
2019-06-14 11:13:17 +0900

    eslint-config-gnavi をバージョンアップ

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,11 @@
 module.exports = {
   extends: ['gnavi'],
-  rules: {
-    semi: ['error', 'never'],
-    'no-magic-numbers': 'off',
-    'max-len': ['error', { code: 120 }],
-    'comma-dangle': ['error', 'always-multiline'],
-    'arrow-parens': ['error', 'as-needed'],
-    'class-methods-use-this': ['error', { exceptMethods: ['init'] }],
-    'import/no-extraneous-dependencies': ['off'],
-    'no-new': 'off',
+  overrides: {
+    files: ['gulp/**/*.js'],
+    rules: {
+      'class-methods-use-this': ['error', { exceptMethods: ['init'] }],
+      'import/no-default-export': 'off',
+      'import/no-extraneous-dependencies': 'off',
+    },
   },
 }
